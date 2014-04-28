eceba6f031ab8ab29603f8833e7d7f2ab4d483cb
Seris <seerriss@gmail.com>
2014-04-28 03:05:59 +0200

    first commit de la revolution

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{	
+	int nya;
+	int i;
+
+	printf("Nya? ");
+	scanf("%d", &nya);
+	for (i = 0; i < nya; ++i)
+	{
+		printf("Nya-nya nya-nya nihao nya coda tsugeraha tsude karu saa !\n");
+	}
+
+	printf("\nDONE!\n");
+	return 0;
+}
